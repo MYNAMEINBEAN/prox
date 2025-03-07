c0c65d46c9af8c84f0e26406172450bd8308b694
MYNAMEINBEAN <bmb6@birmingham.k12.mi.us>
2025-03-06 19:08:30 -0500

    Create obfuscated_iframe.js

---
+++ obfuscated_iframe.js
@@ -0,0 +1,9 @@
+// obfuscated_iframe.js
+(function() {
+    var a = document.createElement('iframe');
+    a.src = 'https://example.com';
+    a.width = '600';
+    a.height = '400';
+    a.title = 'Embedded Content';
+    document.body.appendChild(a);
+})();
