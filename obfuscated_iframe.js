0dc1997d9f6cc553fae658989be6f07d812d2849
MYNAMEINBEAN <bmb6@birmingham.k12.mi.us>
2025-03-06 19:12:26 -0500

    Delete obfuscated_iframe.js

--- obfuscated_iframe.js
+++
@@ -1,9 +0,0 @@
-// obfuscated_iframe.js
-(function() {
-    var a = document.createElement('iframe');
-    a.src = 'https://example.com';
-    a.width = '600';
-    a.height = '400';
-    a.title = 'Embedded Content';
-    document.body.appendChild(a);
-})();
